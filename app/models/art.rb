e08a1ddffd5b5c5ecaaa289c0dae2de7a4b56b01
betel <betel.samson1@gmail.com>
2015-03-27 11:03:50 +0000

    Latest Commit

--- app/models/art.rb
+++ app/models/art.rb
@@ -1,2 +1,10 @@
 class Art < ActiveRecord::Base
+    validates :art_id, presence: true
+   validates :name, presence: true
+    validates :description, presence: true
+    validates :catagory, presence: true
+    validates :size, presence: true
+    validates :price, presence: true
+    validates :url, presence: true
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
+ 
+  get '/about' => 'site#about'
+  get '/contact' => 'site#contact' 
+  get '/gallery' => 'site#gallery' 
+  
+
   resources :arts
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'arts#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
